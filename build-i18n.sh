764fd37d8a47b5bae8bf1e5f3e30a8960dbe0854
Gildardo Bautista <gil.cano@gmail.com>
2014-03-10 13:56:36 -0600

    move and update i18n script

---
+++ build-i18n.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+I18NDUDE=../../bin/i18ndude
+I18NPATH=src/matem/solicitudes
+DOMAIN=matem.solicitudes
+$I18NDUDE rebuild-pot --pot $I18NPATH/locales/$DOMAIN.pot --create $DOMAIN $I18NPATH
+$I18NDUDE sync --pot $I18NPATH/locales/$DOMAIN.pot $I18NPATH/locales/*/LC_MESSAGES/$DOMAIN.po
+$I18NDUDE sync --pot $I18NPATH/locales/plone-manual.pot $I18NPATH/locales*/LC_MESSAGES/plone.po
+
+# domain=matem.solicitudes
+# i18ndude rebuild-pot --pot $domain.pot --create $domain ../
+# i18ndude sync --pot $domain.pot */LC_MESSAGES/$domain.po
+
+# i18ndude sync --pot plone-manual.pot */LC_MESSAGES/plone.po
