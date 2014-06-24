2db4b35dbd0a15834f954f62f05300ef01203b57
Gildardo Bautista <gil.cano@gmail.com>
2014-06-24 14:50:34 -0500

    move doc files to new format. update setup.py file

--- setup.py
+++ setup.py
@@ -1,35 +1,45 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup, find_packages
+"""Installer for this package."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+import os
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 version = '3.0.9'
 
+long_description = \
+    read('docs', 'README.rst') + \
+    read('docs', 'CHANGES.rst') + \
+    read('docs', 'LICENSE.rst')
 
 setup(
     name='matem.solicitudes',
     version=version,
     description="Applications for institutional resources.",
-    long_description=open('README.txt').read() + '\n' +
-    open('docs/CHANGES.txt').read(),
-    # Get more strings from
-    #http://www.python.org/pypi?%3Aaction=list_classifiers
+    long_description=long_description,
+    # Get more from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Environment :: Web Environment',
-        'Framework :: Plone',
-        'License :: OSI Approved :: GNU General Public License (GPL)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        "Framework :: Plone",
+        "Programming Language :: Python",
     ],
-    platforms='Any',
-    author='Instituto de Matematicas - UNAM',
-    author_email='gil@matem.unam.mx',
-    url='http://www.matem.unam.mx',
+    keywords='Plone Python',
+    author='Informática Académica',
+    author_email='computoacademico@im.unam.mx',
+    url='https://github.com/imatem/matem.solicitudes',
     license='GPL',
-    namespace_packages=['matem'],
     packages=find_packages('src', exclude=['ez_setup']),
+    namespace_packages=['matem'],
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
     install_requires=[
+        'Pillow',
+        'Plone',
         'setuptools',
         'Products.FacultyStaffDirectory',
         'Products.ATCountryWidget',
@@ -42,12 +52,16 @@ setup(
     extras_require={
         'develop': [
             'flake8',
-            'jarn.mkrelease',
-            'manuel',
+            'plone.app.debugtoolbar',
+            'plone.reload',
+            'Products.PDBDebugMode',
+            'Products.PrintingMailHost',
             'Sphinx',
-            'zest.releaser',
+            'zptlint',
         ],
         'test': [
+            'mock',
+            'plone.app.robotframework',
             'plone.app.testing',
             'unittest2',
         ],
