0a6311cbfb434afc490dab369b54f04ca64e6977
gil-cano <gil.cano@gmail.com>
2014-08-13 00:26:12 -0500

    Search work with state and kind

--- src/matem/solicitudes/browser/search.py
+++ src/matem/solicitudes/browser/search.py
@@ -32,153 +32,141 @@ class SearchView(BrowserView):
         return self.globaltemplate()
 
     def search(self):
-        form = self.request.form
-        req = self.request
-
-        queryObj=self.queryObj
-
-        applications=[]
-
+        queryObj = self.queryObj
+        applications = []
         try:
-            searchStr=[ self.request.form['Creator'], #0
-                        self.request.form['Pais'],    #1
-                        self.request.form['Ciudad'].lower(),#2
-                        self.request.form['Institucion'].lower(),#3
-                        self.request.form['Desde'],#4
-                        self.request.form['Hasta'],#5
-                        self.request.form['Objeto'].lower(),#6
-                        self.request.form['Area'],#7
-                        self.request.form['TTrabajo'].lower(),#8
-                        self.request.form['Becario'],#9
-                        self.request.form['Asesor'],#10
-                        self.request.form['Cantidad'],#11
-                        self.request.form['CantidadLarger'],#12
-                        self.request.form['CantidadLower']]#13
+            searchStr = [
+                self.request.form['Creator'],
+                self.request.form['Pais'],
+                self.request.form['Ciudad'].lower(),
+                self.request.form['Institucion'].lower(),
+                self.request.form['Desde'],
+                self.request.form['Hasta']
+            ]
 
-            Creator = self.request.form['Creator'] #0
-
-            unTipo=self.request.form['Type']
-            unEstado=self.request.form['State']
+            Creator = self.request.form['Creator']
+            unTipo = self.request.form['Type']
+            unEstado = self.request.form['State']
 
             if 'revision' in unEstado:
-                unEstado=('preeliminar','revisioncomision','revisionconsejo')
+                unEstado = (
+                    'preeliminar',
+                    'revisioncomision',
+                    'revisionconsejo'
+                )
 
         except:
             return applications
 
-        zeroDate=DateTime(1920,1,1)
+        zeroDate = DateTime(1920, 1, 1)
 
         try:
-            fechaDesde=DateTime(int(self.request.form['Desde_year']),int(self.request.form['Desde_month']),int(self.request.form['Desde_day']))
+            fechaDesde = DateTime(
+                int(self.request.form['Desde_year']),
+                int(self.request.form['Desde_month']),
+                int(self.request.form['Desde_day']))
         except:
-            fechaDesde=zeroDate
+            fechaDesde = zeroDate
 
-        encontradas=[]
+        encontradas = []
 
         if 'any' in unEstado:
-            if 'todos' in Creator:
-                encontradas = queryObj.getCurrentFolderApplications()
-            else:
+            if Creator:
                 encontradas = queryObj.getUserApplications(Creator)
-        else:
-            if 'todos' in Creator:
-                encontradas = queryObj.getFolderApplicationsByState(unEstado)
             else:
-                encontradas = queryObj.getFolderApplicationsByStateAndUser(unEstado,Creator)
-
+                encontradas = queryObj.getCurrentFolderApplications()
+        else:
+            if Creator:
+                encontradas = queryObj.getFolderApplicationsByStateAndUser(unEstado, Creator)
+            else:
+                encontradas = queryObj.getFolderApplicationsByState(unEstado)
 
         for obj in encontradas:
-            append=False
+            append = True
+            # try:
+            #     if obj['owner_id'].find(searchStr[0]) != -1 and searchStr[0] is not "":
+            #         append = True
+            #     elif searchStr[0]=='todos':
+            #         append=True
+            #     elif obj['country_code'][0].find(searchStr[1])!=-1 and searchStr[1] is not "":
+            #         append=True
+            #     elif obj['country'].lower().find(searchStr[2])!=-1 and searchStr[2] is not "":
+            #         append=True
+            #     elif obj['institution'].lower().find(searchStr[3])!=-1 and searchStr[3] is not "":
+            #         append=True
+            #     elif obj['from'] >= fechaDesde and fechaDesde != zeroDate:
+            #         if fechaHasta == zeroDate :
+            #             append=True
+            #         else:
+            #             if obj['to'] <= fechaHasta:
+            #                 append=True
+            #     elif obj['to'] <= fechaHasta and fechaHasta != zeroDate:
+            #         if fechaDesde == zeroDate :
+            #             append=True
+            #         else:
+            #             if obj['from'] >= fechaDesde:
+            #                 append=True
+            #     elif obj['objective'].lower().find(searchStr[6])!=-1 and searchStr[6] is not "":
+            #         append=True
+            #     elif obj['special_fields']['work_title'].lower().find(searchStr[8])!=-1 and searchStr[8] is not "":
+            #         append=True
+            # except:
+            #     append=False
 
-            try:
 
-                if obj['owner_id'].find(searchStr[0])!=-1 and searchStr[0] is not "":
-                    append=True
-                elif searchStr[0]=='todos':
-                    append=True
-                elif obj['country_code'][0].find(searchStr[1])!=-1 and searchStr[1] is not "":
-                    append=True
-                elif obj['country'].lower().find(searchStr[2])!=-1 and searchStr[2] is not "":
-                    append=True
-                elif obj['institution'].lower().find(searchStr[3])!=-1 and searchStr[3] is not "":
-                    append=True
-                elif obj['from'] >= fechaDesde and fechaDesde != zeroDate:
-                    if fechaHasta == zeroDate :
-                        append=True
-                    else:
-                        if obj['to'] <= fechaHasta:
-                            append=True
-                elif obj['to'] <= fechaHasta and fechaHasta != zeroDate:
-                    if fechaDesde == zeroDate :
-                        append=True
-                    else:
-                        if obj['from'] >= fechaDesde:
-                            append=True
-                elif obj['objective'].lower().find(searchStr[6])!=-1 and searchStr[6] is not "":
-                    append=True
-                elif obj['special_fields']['work_title'].lower().find(searchStr[8])!=-1 and searchStr[8] is not "":
-                    append=True
-            except:
-                append=False
+            # if obj['meta_type'] == 'SolicitudBecario':
 
-            for area in obj['research_areas']:
-                if area.find(searchStr[7])!=-1 and searchStr[7] is not "":
-                    append=True
+            #     if obj['owner_id'].find(searchStr[9])!=-1 and searchStr[9] is not "":
+            #         append=True
+            #     if obj['special_fields']['researcher_id'].find(searchStr[10])!=-1 and searchStr[10] is not "":
+            #         append=True
 
-            if obj['meta_type'] == 'SolicitudBecario':
+            # try:
+            #     equal=float(searchStr[11])
+            #     larger=float(searchStr[12])
+            #     lower=float(searchStr[13])
+            # except:
+            #     equal=larger=lower=0.0
 
-                if obj['owner_id'].find(searchStr[9])!=-1 and searchStr[9] is not "":
-                    append=True
-                if obj['special_fields']['researcher_id'].find(searchStr[10])!=-1 and searchStr[10] is not "":
-                    append=True
+            # if obj['workflow_state']=="aprobada":
+            #     referencia=obj['total_approved_quantity']
+            # else:
+            #     referencia=obj['total_quantity']
 
-            try:
-                equal=float(searchStr[11])
-                larger=float(searchStr[12])
-                lower=float(searchStr[13])
-            except:
-                equal=larger=lower=0.0
+            # if equal == referencia and equal > 0:
+            #     append=True
+            # elif referencia > larger and larger > 0:
+            #     append=True
+            # elif referencia < lower and lower > 0:
+            #     append=True
 
-            if obj['workflow_state']=="aprobada":
-                referencia=obj['total_approved_quantity']
-            else:
-                referencia=obj['total_quantity']
-
-            if equal == referencia and equal > 0:
-                append=True
-            elif referencia > larger and larger > 0:
-                append=True
-            elif referencia < lower and lower > 0:
-                append=True
-
-            if append and unTipo is not None:
+            if append and unTipo != "all":
                 if unTipo == "becario":
                     if not obj['meta_type'] == "SolicitudBecario":
-                        append=False
+                        append = False
                 elif unTipo == "visitante":
                     if not obj['meta_type'] == "SolicitudVisitante":
-                        append=False
+                        append = False
                 elif unTipo == "licencia":
                     if not obj['meta_type'] == "Solicitud":
-                        append=False
+                        append = False
 
-            if append and unEstado is not None:
-                if unEstado == "aprobada" and not obj['workflow_state']=="aprobada":
-                        append=False
-                elif unEstado == "rechazada" and not obj['workflow_state']=="rechazada":
-                        append=False
-                elif unEstado == "revision" and obj['workflow_state']=="aprobada":
-                        append=False
-                elif unEstado == "revision" and obj['workflow_state']=="rechazada":
-                        append=False
+            # if append and unEstado is not None:
+            #     if unEstado == "aprobada" and not obj['workflow_state']=="aprobada":
+            #             append=False
+            #     elif unEstado == "rechazada" and not obj['workflow_state']=="rechazada":
+            #             append=False
+            #     elif unEstado == "revision" and obj['workflow_state']=="aprobada":
+            #             append=False
+            #     elif unEstado == "revision" and obj['workflow_state']=="rechazada":
+            #             append=False
 
             if append:
                 applications.append(obj)
 
-            append=False
-        sorted_list = [(a['revision_ci_date'], a) for a in applications] 
+        sorted_list = [(a['revision_ci_date'], a) for a in applications]
         sorted_list.sort(key=lambda  x: x[0])
-        return [dic for name, dic in sorted_list] 
+        return [dic for name, dic in sorted_list]
 
     def searchSlow(self):
         form = self.request.form
@@ -261,7 +249,7 @@ class SearchView(BrowserView):
                     append=True
 
             if obj['meta_type'] == 'SolicitudBecario':
-            
+
                 if obj['owner_id'].find(searchStr[9])!=-1 and searchStr[9] is not "":
                     append=True
                 if obj['special_fields']['researcher_id'].find(searchStr[10])!=-1 and searchStr[10] is not "":
