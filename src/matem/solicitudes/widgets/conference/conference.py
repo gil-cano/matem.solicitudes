4548628cd36b4060f09464cc1f789ecb88f635e6
adriana-rv <adriana.rv@gmail.com>
2016-02-18 11:37:20 -0600

    Add conference field and widget

---
+++ src/matem/solicitudes/widgets/conference/conference.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from Products.DataGridField import DataGridField
+from archetypes.schemaextender.field import ExtensionField
+from Products.DataGridField import DataGridWidget
+from AccessControl import ClassSecurityInfo
+from Products.Archetypes.Registry import registerWidget
+from matem.solicitudes import solicitudesMessageFactory as _
+
+
+class ConferenceWidget(DataGridWidget):
+    _properties = DataGridWidget._properties.copy()
+    _properties.update({
+        'macro': 'grid_widget',
+        'helper_css': ('solwidgets.css',),
+        # 'helper_js': ('course.js',),
+    })
+
+    security = ClassSecurityInfo()
+
+registerWidget(
+    ConferenceWidget,
+    title='ConferenceWidget',
+    description=('Widget for display course on DataGridConferenceField type format'),
+    used_for=('matem.solicitudes.widgets.conference.conference.DataGridConferenceField',)
+)
+
+
+class DataGridConferenceField(ExtensionField, DataGridField):
+
+    _properties = DataGridField._properties.copy()
+    _properties.update({
+        'type': 'datagridconferencefield',
+        # 'validators': DateFreeValidator(),
+        'widget': ConferenceWidget,
+        # 'rows': [],
+        'label_item': _(u'Conference'),
+        'label_button': _(u'+ Conference'),
+    })
