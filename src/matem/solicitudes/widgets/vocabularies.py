70a4247a985600f1a77380cf57eabb3ce665a77e
adriana-rv <adriana.rv@gmail.com>
2016-02-25 17:38:38 -0600

    Add vocabularies for widgets

--- src/matem/solicitudes/widgets/vocabularies.py
+++ src/matem/solicitudes/widgets/vocabularies.py
@@ -4,10 +4,15 @@ from Products.Archetypes.interfaces import IVocabulary
 from Products.Archetypes.public import DisplayList
 from zope.interface import implements
 
+from zope.interface import directlyProvides
+from zope.schema.interfaces import IVocabularyFactory
+from zope.schema.vocabulary import SimpleTerm
+from zope.schema.vocabulary import SimpleVocabulary
+
 # Vocabularies for the widgets
 
 
-class ConferenceTypeVocabulary:
+class EventTypeVocabulary:
     implements(IVocabulary)
 
     def getDisplayList(self, instance):
@@ -16,7 +21,18 @@ class ConferenceTypeVocabulary:
             ('seminary', _(u'Seminary')),
             ('coloquio', _(u'Coloquio')),
             ('school', _(u'School')),
+            ('workshop', _('Workshop')),
             ('other', _(u'Other')),
+        ])
+
+
+class BooleanTypeVocabulary:
+    implements(IVocabulary)
+
+    def getDisplayList(self, instance):
+        return DisplayList([
+            ('yes', _(u'Yes')),
+            ('no', _(u'No')),
         ])
 
 
@@ -28,6 +44,26 @@ class ConferenceAssistantVocabulary:
             ('invitation', _(u'By Invitation')),
             ('application', _(u'By Application')),
         ])
+
+
+def ConferenceTypeVocabulary(context):
+    """Vocabulary factory for conference type"""
+    return SimpleVocabulary([
+        SimpleTerm(value='research', title=_(u'Research')),
+        SimpleTerm(value='divulgation', title=_(u'Divulgation')),
+        SimpleTerm(value='human_resources', title=_(u'Human resources training')),
+    ])
+directlyProvides(ConferenceTypeVocabulary, IVocabularyFactory)
+
+# class ConferenceTypeVocabulary:
+#     implements(IVocabulary)
+
+#     def getDisplayList(self, instance):
+#         return DisplayList([
+#             ('research', _(u'Research')),
+#             ('divulgation', _(u'Divulgation')),
+#             ('human_resources', _(u'Human resources training')),
+#         ])
 
 
 class CourselevelVocabulary:
