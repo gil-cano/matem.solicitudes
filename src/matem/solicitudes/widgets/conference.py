1e183c7ab5a93b26bd96eb14e5246286b67cd7b4
adriana-rv <adriana.rv@gmail.com>
2016-03-02 14:42:34 -0600

    Fix multiselect values

--- src/matem/solicitudes/widgets/conference.py
+++ src/matem/solicitudes/widgets/conference.py
@@ -5,6 +5,7 @@ from matem.solicitudes import solicitudesMessageFactory as _
 from Products.Archetypes.Registry import registerWidget
 from Products.DataGridField import DataGridField
 from Products.DataGridField import DataGridWidget
+from collective.datagridcolumns.MultiSelectColumn import MultiSelectColumn
 
 
 class ConferenceWidget(DataGridWidget):
@@ -16,6 +17,40 @@ class ConferenceWidget(DataGridWidget):
     })
 
     security = ClassSecurityInfo()
+
+    security.declarePublic('getResquestValues')
+
+    def getResquestValues(self, form, value, context, field, columnId):
+        columndef = self.getColumnDefinition(field, columnId)
+
+        newValue = []
+        if type(columndef) is not MultiSelectColumn:
+            return newValue
+
+        if not form.has_key(field.getName()):
+            return newValue
+
+        for row in value:
+
+            # we must clone row since
+            # row is readonly ZPublished.HTTPRequest.record object
+            newRow = {}
+            for key in row.keys():
+                newRow[key] = row[key]
+
+            orderIndex = row["orderindex_"]
+            # pageColumns.options.required.64
+
+            newRow[columnId] = []
+            for vitem in columndef.getVocabulary(context).keys():
+                cellId = "%s.%s.%s.%s" % (field.getName(), columnId, vitem, orderIndex)
+                if form.has_key(cellId):
+                    # If radio button is set in HTML form
+                    # it's id appears in form of field.column.orderIndex
+                    newRow[columnId].append(vitem)
+
+            newValue.append(newRow)
+        return newValue
 
 registerWidget(
     ConferenceWidget,
