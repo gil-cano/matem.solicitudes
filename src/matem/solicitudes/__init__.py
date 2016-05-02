32f975a7e43cc646ba6cf7032d19280207d4e604
gil-cano <gil.cano@gmail.com>
2016-05-02 16:18:28 -0500

    dont sort init file

--- src/matem/solicitudes/__init__.py
+++ src/matem/solicitudes/__init__.py
@@ -7,16 +7,14 @@ from Products.validation import validation  # noqa
 from matem.solicitudes.content.validators import StartBeforeEnd  # noqa
 validation.register(StartBeforeEnd('isGreaterthanStart'))  # noqa
 
-
-from Products.Archetypes.atapi import listTypes
-from Products.Archetypes.atapi import process_types
-from Products.CMFCore import utils
-
 from matem.solicitudes.content.solicitud import Solicitud  # noqa
 from matem.solicitudes.content.solicitudbecario import SolicitudBecario  # noqa
 from matem.solicitudes.content.solicitudfolder import SolicitudFolder  # noqa
 from matem.solicitudes.content.solicitudinstitucional import SolicitudInstitucional  # noqa
 from matem.solicitudes.content.solicitudvisitante import SolicitudVisitante  # noqa
+from Products.Archetypes.atapi import listTypes
+from Products.Archetypes.atapi import process_types
+from Products.CMFCore import utils
 
 import config
 
@@ -29,7 +27,7 @@ def initialize(context):
 
     for atype, constructor in zip(content_types, constructors):
         utils.ContentInit(
-            '%s: %s' % (config.PROJECTNAME, atype.portal_type),
+            '{0}: {1}'.format(config.PROJECTNAME, atype.portal_type),
             content_types=(atype, ),
             permission=config.ADD_CONTENT_PERMISSION[atype.portal_type],
             extra_constructors=(constructor, ),
