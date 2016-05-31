cad5b68c257a4196860b5175f7bf9a8486219015
adriana-rv <adriana.rv@gmail.com>
2016-05-31 10:50:05 -0500

    Update assistance js

--- src/matem/solicitudes/skins/matem.solicitudes/datagrid_assistance.js
+++ src/matem/solicitudes/skins/matem.solicitudes/datagrid_assistance.js
@@ -1,48 +1,54 @@
 /**
- * JavaScript for the history widgets
+ * JavaScript for the history widgets assistance
  *
- * disabled
- * readonly
- * pointer-events
- $(".row-neditable").closest('tr').find('input:text').attr('disabled', 'disabled')
- $(".row-neditable").closest('tr').find('select').attr('disabled', 'disabled')
-
-  $('.row-neditable').closest('tr').find('input:text').css('pointer-events','none');
-  $('.row-neditable').closest('tr').find('select').css('pointer-events','none');
-
-
-  academic_type = $("#eventtype_assistance_0").val()
-  $("#othereventtype_assistance_0").show()
-  $("#othereventtype_assistance_0").hide()
-  $("#othereventtype_assistance_0").parent().show()
-
-  $("#eventtype_assistance_0").attr('onchange', 'othervisibility()')
-
-
-  $("#datagridwidget-tbody-assistance").children().length
-
-
-
-
+ *
  */
 
 (function($) {
   $(document).ready(function () {
 
-    /*var emptyrow = $("#datagridwidget-tbody-assistance").children().last()*/
     $("#institucion").change(function() {
       var general_institution = $("#institucion").val()
       $("#institution_assistance_new").val(general_institution);
     });
 
-    /*var general_institution = $("#institucion").val()*/
-    var year = $("#edit_form_fecha_desde_0_year").val()
-    var month = $("#edit_form_fecha_desde_0_month").val()
-    var day = $("#edit_form_fecha_desde_0_day").val()
+    $("#edit_form_fecha_desde_0_year").change(function() {
+      var year = $("#edit_form_fecha_desde_0_year").val()
+      var month = $("#edit_form_fecha_desde_0_month").val()
+      var day = $("#edit_form_fecha_desde_0_day").val()
+      var newdate = day + '/' + month + '/' + year
+      $("#assistancedate_assistance_new").val(newdate);
+    });
 
+    $("#edit_form_fecha_desde_0_month").change(function() {
+      var year = $("#edit_form_fecha_desde_0_year").val()
+      var month = $("#edit_form_fecha_desde_0_month").val()
+      var day = $("#edit_form_fecha_desde_0_day").val()
+      var newdate = day + '/' + month + '/' + year
+      $("#assistancedate_assistance_new").val(newdate);
+    });
 
-    /* For master select */
-    $("#datagridwidget-tbody-assistance").children().length;
+    $("#edit_form_fecha_desde_0_day").change(function() {
+      var year = $("#edit_form_fecha_desde_0_year").val()
+      var month = $("#edit_form_fecha_desde_0_month").val()
+      var day = $("#edit_form_fecha_desde_0_day").val()
+      var newdate = day + '/' + month + '/' + year
+      $("#assistancedate_assistance_new").val(newdate);
+    });
+
+    /*$("#edit_form_fecha_desde_0").change(function() {
+      console.log('Entra hide !!!')
+      var hidedate = $("#edit_form_fecha_desde_0").val()
+      var splitdate = hidedate.split('-')
+      var year = splitdate[0]
+      var month = splitdate[1]
+      var day = splitdate[2]
+      var newdate = day + '/' + month + '/' + year
+      $("#assistancedate_assistance_new").val(newdate);
+    });*/
+
+    /* For master select*/
+    /*$("#datagridwidget-tbody-assistance").children().length;
     console.log('Entra!!!')
     $("#eventtype_assistance_1").change(function() {
       academic_type = $("#eventtype_assistance_1").val()
@@ -50,6 +56,6 @@
         $("#othereventtype_assistance_1").parent().show()
       else
         $("#othereventtype_assistance_1").parent().hide()
-    });
+    });*/
   });
 })(jQuery);
