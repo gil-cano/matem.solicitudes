c6447844cb0c30c8a4a0f57042ebbbec7507d859
adriana-rv <adriana.rv@gmail.com>
2019-09-04 14:53:14 -0500

    Show the view woth restricted permissions

--- src/matem/solicitudes/browser/etyview.py
+++ src/matem/solicitudes/browser/etyview.py
@@ -1,21 +1,10 @@
 #-*- coding: utf-8 -*-
+from DateTime import DateTime
+from operator import itemgetter
+from plone import api
+from plone.i18n.normalizer import idnormalizer as idn
 from Products.CMFCore.utils import getToolByName
 from Products.Five import BrowserView
-# from UNAM.imateCVct import _
-# from UNAM.imateCVct.vocabularies import CURSOSEMUNAM
-# from collections import OrderedDict
-# from operator import itemgetter
-# from plone.i18n.normalizer import idnormalizer as idn
-# from zope.component import getUtility
-# from zope.component.hooks import getSite
-# from zope.i18n import translate
-# from zope.schema.interfaces import IVocabularyFactory
-# import collections
-from DateTime import DateTime
-from plone.i18n.normalizer import idnormalizer as idn
-from operator import itemgetter
-
-from plone import api
 
 
 class EtyView(BrowserView):
@@ -23,8 +12,19 @@ class EtyView(BrowserView):
     def __init__(self, context, request):
         self.context = context
         self.request = request
-    #     self.atype = self.context.portal_type.replace('Folder', '')
 
+    def __call__(self):
+
+        if self.context.id == 'licencias-y-comisiones':
+            userid = api.user.get_current().id
+
+            pm = getToolByName(self.context, 'portal_membership')
+            roles_in_context = pm.getAuthenticatedMember().getRolesInContext(self.context)
+            if 'Reader' in self.context.get_local_roles_for_userid(userid) or 'Manager' in roles_in_context:
+                return self.index()
+
+        myportal = self.context.portal_url.getPortalObject()
+        return myportal.restrictedTraverse('insufficient_privileges')()
 
     def staff_on_leave_or_commission_days(self):
 
@@ -34,25 +34,25 @@ class EtyView(BrowserView):
         today = DateTime('/'.join([str(ftoday.year()), str(ftoday.month()), str(ftoday.day())]))
         start_date = today
         # end_date = today + 9.9999
-        end_date = today + 10
+        end_date = today + 1
 
-        brains = api.content.find(
-            Type='Solicitud de Licencia/Comision',
-            review_state='aprobada',
-            fecha_hasta={'query': [start_date, ], 'range': 'min'},
-            fecha_desde={'query': [end_date, ], 'range': 'max'},
-            sort_on='sortable_title',
-        )
-        for b in brains:
-            obj = b.getObject()
-            # researchers.append((b.Title, b.fecha_desde.strftime('%d/%m/%Y'), b.fecha_hasta.strftime('%d/%m/%Y')))
-            researchers.append((obj.nombre_owner, b.fecha_desde.strftime('%d/%m/%Y'), b.fecha_hasta.strftime('%d/%m/%Y')))
+        with api.env.adopt_user(username='admin'):
+
+            brains = api.content.find(
+                Type='Solicitud de Licencia/Comision',
+                review_state='aprobada',
+                fecha_hasta={'query': [start_date, ], 'range': 'min'},
+                fecha_desde={'query': [end_date, ], 'range': 'max'},
+                sort_on='sortable_title',
+            )
+            for b in brains:
+                obj = b.getObject()
+                researchers.append((obj.nombre_owner, b.fecha_hasta.strftime('%d/%m/%Y')))
 
         aux = [(r, idn.normalize(r[0]), ) for r in researchers]
         aux_sorted = sorted(aux, key=itemgetter(1))
         sortresearchers = [t[0] for t in aux_sorted]
 
-        # return researchers
         return sortresearchers
 
     def staff_on_sabbatical(self):
